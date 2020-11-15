1b7a0d7de3aeee8ff63ae62fb05c9017f2faea30
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2020-11-15 15:28:36 +0530

    Add files via upload

---
+++ Check_Input.py
@@ -0,0 +1,10 @@
+# https://www.geeksforgeeks.org/program-check-input-integer-string/
+# check input
+st=str(input())
+if(str.isdigit(st)==True):
+    print("Integer")
+else:
+    print("String")
+
+
+    
