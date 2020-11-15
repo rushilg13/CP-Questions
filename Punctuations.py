046ff3b2a87d9b5fded8c89f15ad3081779bc13d
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2020-11-15 15:08:44 +0530

    Update Punctuations.py

--- Punctuations.py
+++ Punctuations.py
@@ -1,7 +1,6 @@
 # Remove Punctuations
 # https://www.geeksforgeeks.org/removing-punctuations-given-string/
 st=str(input())
-count=0
 for i in '''!"#$%&'()*+,-./:;?@[\]^_`{|}~<>''':
     if(i in st):
         st=st.replace(i,'')
