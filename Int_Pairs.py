4b32c518333161386c4c1cbc3b4b1b1269b32bd4
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2020-11-16 00:12:23 +0530

    Add files via upload

---
+++ Int_Pairs.py
@@ -0,0 +1,11 @@
+# All Pairs of integer array whose sum is equal to target number
+num=list(map(int, input().split()))
+taregt=int(input())
+ans=[]
+for i in range(len(num)):
+    for j in range(i, len(num)):
+        if(num[i]+num[j]==taregt):
+            ans.append(num[i])
+            ans.append(num[j])
+            print(ans)
+            ans=[]
