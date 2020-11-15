faf67c60ee0e4eefcc9bd7d3522f70118fb67b62
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2020-11-15 14:33:44 +0530

    Update Pangram.py

--- Pangram.py
+++ Pangram.py
@@ -5,8 +5,10 @@ for i in 'qwertyuiopasdfghjklzxcvbnm':
     if(i in st):
         flag=1
     else:
-        print("Not Pangram")
+        flag=0
         break
 if(flag==1):
     print("Pangram")
+else:
+    print("Not Pangram")
     
