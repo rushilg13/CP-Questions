ecce2bc98ea9f37b7dc55e585b27c24e669750b0
Rushil Goel <55556907+rushilg13@users.noreply.github.com>
2020-11-20 20:47:27 +0530

    Add files via upload

---
+++ PerfSq.py
@@ -0,0 +1,8 @@
+# Find number of perf sq in a range
+n1=int(input())
+n2=int(input())
+count=0
+for i in range(n1,n2+1):
+    if((int(i**0.5))==int((i**0.5)**2)**0.5):
+        count+=1
+print(count)
